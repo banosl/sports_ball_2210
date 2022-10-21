88dfee450da04d0675abcc18c266cd1d34958f4d
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:36:36 -0600

    Feat: added player_count attribute to Team

--- lib/team.rb
+++ lib/team.rb
@@ -1,11 +1,13 @@
 class Team
     attr_reader :team_name,
                 :team_home,
-                :roster
+                :roster,
+                :player_count
 
     def initialize (team_name, team_home)
         @team_name = team_name
         @team_home = team_home
         @roster = []
+        @player_count = @roster.count
     end
 end
