2080b25584144be5376ad741d635fec09bc73385
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:39:41 -0600

    Test: wrote test for checking the new player total

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -14,21 +14,22 @@ RSpec.describe Team do
         expect(team.roster).to eq([])
     end
 
-    xit 'team player count is 0' do
+    it 'team player count is 0' do
         team = Team.new("Dodgers", "Los Angeles")
 
         expect(team.player_count).to eq(0)
     end
 
-    xit 'team roster includes two players' do
+    it 'team roster includes two players' do
         team = Team.new("Dodgers", "Los Angeles")
 
         player_1 = Player.new("Michael Palledorous" , 1000000, 36)
-        player_2 = Player.new("Kenny DeNunez" , 500000, 24.)
+        player_2 = Player.new("Kenny DeNunez" , 500000, 24)
 
         team.add_player(player_1)
         team.add_player(player_2)
-
+require 'pry'; binding.pry
         expect(team.roster).to eq([player_1, player_2])
+        expect(team.player_count).to eq(2)
     end
 end
