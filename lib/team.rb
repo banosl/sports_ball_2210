5c4edef311a12a12c0a92ebdb16ce47c548e0d57
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:45:34 -0600

    Fix created a player_count method instead of having it as an attribute

--- lib/team.rb
+++ lib/team.rb
@@ -1,18 +1,20 @@
 class Team
     attr_reader :team_name,
                 :team_home,
-                :roster,
-                :player_count
+                :roster
 
     def initialize (team_name, team_home)
         @team_name = team_name
         @team_home = team_home
         @roster = []
-        @player_count = @roster.count
     end
 
     def add_player(new_player)
         @roster << new_player
+    end
+
+    def player_count
+        @roster.count
     end
 
 end
