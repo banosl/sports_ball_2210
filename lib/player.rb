e8966516e5564d124a8b689121c8a53638295f9c
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:03:56 -0600

    Fix: corrected initialize method

--- lib/player.rb
+++ lib/player.rb
@@ -3,9 +3,9 @@ class Player
                 :monthly_cost,
                 :contract_length
 
-initialize (name, monthly_cost, contract_length)
-    @name = name
-    @monthly_cost = monthly_cost
-    @contract_length = contract_length
-
+    def initialize (name, monthly_cost, contract_length)
+        @name = name
+        @monthly_cost = monthly_cost
+        @contract_length = contract_length
+    end
 end
