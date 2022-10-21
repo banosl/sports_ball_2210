96d6d8f8d4506871bc0447c950cfa566a3bd8706
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:18:35 -0600

    Test: added test for #set_nickname
    q

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -44,4 +44,12 @@ RSpec.describe Player do
     expect(player.nickname).to eq(nil)
   end
 
+  it 'nickname is added' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    player.set_nickname!("Squints")
+
+    expect(player.nickname).to eq("Squints")
+  end
+
 end
