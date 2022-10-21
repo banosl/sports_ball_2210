3d0c372abb38e0c6c557c6e44b92f025b831b930
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:25:54 -0600

    Test: wrote test for class Team

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,7 +2,11 @@ require './lib/player'
 require './lib/team'
 
 RSpec.describe Team do
-    
+    it 'team exists' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        expect(team).to be_instance_of(Team)
+    end
 
 
 end
