194bff18e8415d33235c21995accf53e64f83d5b
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:02:27 -0600

    Feat: added class Player

---
+++ lib/player.rb
@@ -0,0 +1,11 @@
+class Player
+    attr_reader :name,
+                :monthly_cost,
+                :contract_length
+
+initialize (name, monthly_cost, contract_length)
+    @name = name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+
+end
