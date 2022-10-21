701d0876789411cb12030ff7859921450caa9efe
Leo Banos Garcia <bsleo625@gmail.com>
2022-10-20 19:35:36 -0600

    Feat: added roster attribute to Team

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,11 @@
 class Team
+    attr_reader :team_name,
+                :team_home,
+                :roster
 
     def initialize (team_name, team_home)
         @team_name = team_name
         @team_home = team_home
+        @roster = []
     end
 end
